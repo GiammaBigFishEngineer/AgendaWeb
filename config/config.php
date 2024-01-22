94bf9b0c63f78dff9b91172d204eef2a8d0a2b0a
Louis Marotta <loumb31@gmail.com>
2024-01-22 11:13:51 +0100

    delete hardcoded config

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Config
-{
-    static string $db_host = '85.31.237.111';
-    static string $db_name = 'Agenda';
-    static string $db_username = 'root';
-    static string $db_password = 'Tempo123!';
-}
