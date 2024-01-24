bfb525eaed88507024c5ee0baf5661fe5509c07c
Louis Marotta <loumb31@gmail.com>
2024-01-24 17:14:43 +0100

    Rewritten router to allow pattern routes (ex. "/api/event/:id")

--- index.php
+++ index.php
@@ -20,7 +20,10 @@ define('__ROOT__', dirname(__FILE__));
 define('__BASEURL__', (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]");
 
 require_once(__ROOT__ . '/models/UserModel.php');
+
 require_once(__ROOT__ . '/controllers/UserController.php');
+require_once(__ROOT__ . '/controllers/EventController.php');
+
 require_once(__ROOT__ . '/config/EnvLoader.php');
 
 session_start();
@@ -31,17 +34,18 @@ class Dispatcher
 {
     private $method;
     private $path;
+    private $path_segments;
 
     public function __construct()
     {
-        $this->method = $_SERVER["REQUEST_METHOD"];
+        $this->method = RequestMethod::convert($_SERVER["REQUEST_METHOD"]);
         $this->path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+        // $this->path_segments = explode('/', $this->path);
     }
     
     public function isRoute(string $method, string $route, array ...$handlers): int
     {
         global $params;
-        // $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
         $route_rgx = preg_replace('#:(\w+)#','(?<$1>(\S+))', $route);
         return preg_match("#^$route_rgx$#", $this->path, $params);
     }
@@ -49,56 +53,114 @@ class Dispatcher
     public function dispatch()
     {
         if(isset($_SESSION["loggedIn"])) {
-            switch ($this->path) {
-                case "/":
+            $routeHandlers = [
+                // "/api/events" => function($params) {
+                //     if ($this->method == RequestMethod::GET) {
+                //         EventController::list();
+                //     }
+                //     if ($this->method == RequestMethod::POST) {
+                //         EventController::create();
+                //     }
+                // },
+                "/api/event/:id" => function($params) {
+                    $eventId = $params['id'];
+                    if ($this->method == RequestMethod::GET) {
+                        EventController::get($eventId);
+                    }
+                    if ($this->method == RequestMethod::DELETE) {
+                        EventController::delete($eventId);
+                    }
+                    if ($this->method == RequestMethod::POST) {
+                        EventController::save($eventId);
+                    }
+                },
+                "/" => function($params) {
                     UserController::showHome();
-                    break;
-                case "/logout":
+                },
+                "/logout" => function($params) {
                     session_unset();
                     header("Location: /login");
-                    break;
-                default:
-                    echo "404 HTML<br>";
-                    echo $this->path;
-                    break;
-            }
+                }
+            ];
         } else {
-            switch ($this->path) {
-                case "/test":
-                    echo "Prova lista utenti";
-                    $list = UserModel::all();
-                    foreach($list as $user){
-                        echo "<br>$user->email";
-                    }
-                    break;
-                case "/login":
-                    if($this->method == "POST"){
+            $routeHandlers = [
+                "/" => function($params) {
+                    header("Location: /login");
+                },
+                "/login" => function($params) {
+                    if ($this->method == RequestMethod::POST) {
                         UserController::login();
-                    } else if ($this->method == "GET"){
+                    }
+                    if($this->method == RequestMethod::GET) {
                         UserController::showLogin();
                     }
-                    break;
-                case "/":
-                    header("Location: /login");
-                    break;
-                default:
-                    echo "404 HTML<br>";
-                    echo $this->path;
-                    break;
+                }
+            ];
+        }
+
+        $this->route($routeHandlers);
+    }
+
+    private function route($routeHandlers){
+        $matched = false;
+    
+        //Cerca se il path attuale corrisponde un pattern
+        foreach ($routeHandlers as $routePattern => $handler) {
+            $params = $this->matchRoutePattern($routePattern);
+
+            if ($params !== null) {
+                $handler($params);
+                $matched = true;
+                break;
+            }
+        }
+    
+        if (!$matched) {
+            $this->page_404();
+        }
+    }
+
+    private function matchRoutePattern($pattern): ?array {
+        $pathSegments = explode('/', $this->path);
+        $patternSegments = explode('/', $pattern);
+    
+        $params = [];
+    
+        //Per ogni segmento del path attuale
+        foreach ($pathSegments as $index => $segment) {
+            //Se questa parte del pattern è un parametro
+            if (isset($patternSegments[$index][0]) && $patternSegments[$index][0] === ":") {
+                $paramName = substr($patternSegments[$index], 1);
+                $params[$paramName] = $segment;
+            //Se il segmento non combacia
+            } else if ($segment !== $patternSegments[$index]) {
+                return null;
             }
         }
 
-
+        return $params; 
     }
-
+    
 
     function json(mixed $data)
     {
-      header('Content-Type: application/json');
-      echo json_encode($data);
-      exit;
+        header('Content-Type: application/json');
+        echo json_encode($data);
+        exit;
     }
 
+    private function page_404()
+    {
+        if ($_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest') {
+            $this->json(['err' => 'Route not found!']);
+        } else {
+            header("HTTP/1.0 404 Not Found");
+            echo "404 HTML<br>";
+            echo $this->path;
+        }
+    }
+
+    //! ONLY AVAILABLE FOR PHP 8.2
     // public function dispatch(){
     //     (match(1) {
     //         $this->isRoute('GET', '/') => function () {
@@ -114,11 +176,31 @@ class Dispatcher
     //         $this->isRoute('GET', '/api/users/:id') => function () {
     //             global $params;
     //             echo(UserController::get($params['id']));
-    //             // $this->json(['id' => ]);
-    //           },
+    //         },
+    //         $this->isRoute('GET', '/api/events/:id') => function () {
+    //             global $params;
+    //             echo(EventController::get($params['id']));
+    //         },
     //         default => fn() => $this->json(['err' => 'Route not found!'])
     //     })();        
     // }
+}
+
+enum RequestMethod: string {
+    case GET = 'GET';
+    case POST = 'POST';
+    case PUT = 'PUT';
+    case DELETE = 'DELETE';
+
+    public static function convert(string $val): RequestMethod {
+        return match ($val) {
+            'GET' => self::GET,
+            'POST' => self::POST,
+            'PUT' => self::PUT,
+            'DELETE' => self::DELETE,
+            default => throw new InvalidArgumentException("Invalid HTTP method: $val"),
+        };
+    }
 }
 
 $dispatcher = new Dispatcher();
